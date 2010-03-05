584c7f0d28e903b69cea5a9698b1e90727101d63
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-03-05 10:57:21 +0000

    * translations  * fix escaping bugs  * fix download bugs
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@2381 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/DE/messages_po.js
+++ locales/DE/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* extesion: , lang: DE */
 var langCode = 'DE';
-var i18n_tr = {"File Manager":"","File Browser":"","Current directory":"","URL":"","Preview":"","Actions":"","Select":"","Root":"","New directory":"","Copy url":"","Download":"","Delete":"","File upload":"","File":"","Name":"","Close":""};
+var i18n_tr = {"File Manager":"Dateimanager","File Browser":"Dateibrowser","Current directory":"Aktuelles Verzeichnis","URL":"URL","Preview":"Vorschau","Actions":"Aktionen","Select":"Ausw","Root":"Root","New directory":"Neues Verzeichnis","Copy url":"URL kopieren","Download":"Download","Delete":"L","File upload":"Datei-Upload","File":"Datei","Name":"Name","Close":"Schlie"};
--- locales/FR/messages_po.js
+++ locales/FR/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* extesion: , lang: FR */
 var langCode = 'FR';
-var i18n_tr = {"File Manager":"","File Browser":"","Current directory":"","URL":"","Preview":"","Actions":"","Select":"","Root":"","New directory":"","Copy url":"","Download":"","Delete":"","File upload":"","File":"","Name":"","Close":""};
+var i18n_tr = {"File Manager":"Gestionnaire de fichiers ","File Browser":"Navigateur","Current directory":"R","URL":"","Preview":"Afficher","Actions":"Actions","Select":"S","Root":"Racine","New directory":"Nouveau r","Copy url":"Copier le lien","Download":"T","Delete":"Supprimer","File upload":"T","File":"Fichier","Name":"Nom","Close":"Fermer"};
