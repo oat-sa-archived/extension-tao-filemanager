8194f67e2bc6368b18cfbf54ba4d68880ac5d2b3
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-03-05 15:57:28 +0000

    * fix bugs on tests  * fix german encoding  * fix tab nav in other lang
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@2395 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/DE/messages_po.js
+++ locales/DE/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* extesion: , lang: DE */
 var langCode = 'DE';
-var i18n_tr = {"File Manager":"Dateimanager","File Browser":"Dateibrowser","Current directory":"Aktuelles Verzeichnis","URL":"URL","Preview":"Vorschau","Actions":"Aktionen","Select":"Ausw","Root":"Root","New directory":"Neues Verzeichnis","Copy url":"URL kopieren","Download":"Download","Delete":"L","File upload":"Datei-Upload","File":"Datei","Name":"Name","Close":"Schlie"};
+var i18n_tr = {"File Manager":"Dateimanager","File Browser":"Dateibrowser","Current directory":"Aktuelles Verzeichnis","URL":"URL","Preview":"Vorschau","Actions":"Aktionen","Select":"Ausw\u00e4hlen","Root":"Root","New directory":"Neues Verzeichnis","Copy url":"URL kopieren","Download":"Download","Delete":"L\u00f6schen","File upload":"Datei-Upload","File":"Datei","Name":"Name","Close":"Schlie\u00dfen"};
--- locales/LU/messages_po.js
+++ locales/LU/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* extesion: , lang: LU */
 var langCode = 'LU';
-var i18n_tr = {"File Manager":"","File Browser":"","Current directory":"","URL":"","Preview":"","Actions":"","Select":"","Root":"","New directory":"","Copy url":"","Download":"","Delete":"","File upload":"","File":"","Name":"","Close":""};
+var i18n_tr = {"":"Dateimanager","File Browser":"Dateibrowser","Current directory":"Aktuellen R\u017dpertoire","URL":"URL","Preview":"Viirbl\u017dck","Actions":"Aktiounen","Select":"Auswielen","Root":"Root","New directory":"Naien R\u017dpertoire","Copy url":"URL kop\u017diren","Download":"Erooflueden","Delete":"L\u0160schen","File upload":"Fichier roplueden","File":"Fichier","Name":"Numm","Close":"Zoumachen"};
