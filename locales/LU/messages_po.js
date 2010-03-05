2e57d3694e92c889666bead280db200e2b5253de
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-03-05 17:03:00 +0000

    * settigns refresh  * languages encoding fixs
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@2402 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/LU/messages_po.js
+++ locales/LU/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* extesion: , lang: LU */
 var langCode = 'LU';
-var i18n_tr = {"":"Dateimanager","File Browser":"Dateibrowser","Current directory":"Aktuellen R\u017dpertoire","URL":"URL","Preview":"Viirbl\u017dck","Actions":"Aktiounen","Select":"Auswielen","Root":"Root","New directory":"Naien R\u017dpertoire","Copy url":"URL kop\u017diren","Download":"Erooflueden","Delete":"L\u0160schen","File upload":"Fichier roplueden","File":"Fichier","Name":"Numm","Close":"Zoumachen"};
+var i18n_tr = {"":"Dateimanager","File Browser":"Dateibrowser","Current directory":"Aktuellen R\u00e9pertoire","URL":"URL","Preview":"Viirbl\u00e9ck","Actions":"Aktiounen","Select":"Auswielen","Root":"Root","New directory":"Naien R\u00e9pertoire","Copy url":"URL kop\u00e9iren","Download":"Erooflueden","Delete":"L\u00e4schen","File upload":"Fichier roplueden","File":"Fichier","Name":"Numm","Close":"Zoumachen"};
