fc72f598db86910e79e7c17166e337a399e47467
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-03-23 11:14:34 +0000

    * update translations
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@2473 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/FR/messages_po.js
+++ locales/FR/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* extesion: , lang: FR */
 var langCode = 'FR';
-var i18n_tr = {"File Manager":"Gestionnaire de fichiers ","File Browser":"Navigateur","Current directory":"R","URL":"","Preview":"Afficher","Actions":"Actions","Select":"S","Root":"Racine","New directory":"Nouveau r","Copy url":"Copier le lien","Download":"T","Delete":"Supprimer","File upload":"T","File":"Fichier","Name":"Nom","Close":"Fermer"};
+var i18n_tr = {"File Manager":"Gestionnaire de fichiers","File Browser":"Navigateur","Current directory":"R","URL":"","Preview":"Afficher","Actions":"Actions","Select":"S","Root":"Racine","New directory":"Nouveau r","Copy url":"Copier le lien","Download":"T","Delete":"Supprimer","File upload":"T","File":"Fichier","Name":"Nom","Close":"Fermer"};
