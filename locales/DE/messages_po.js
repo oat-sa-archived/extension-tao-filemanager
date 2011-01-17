f9ef81399c4e3dd9199bb0a3d3bc208140f4b106
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-17 11:55:28 +0000

    Update translations files, complete FR locale files and so fix the Bug #375
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@4198 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/DE/messages_po.js
+++ locales/DE/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
-/* extesion: , lang: DE */
+/* extesion: filemanager, lang: DE */
 var langCode = 'DE';
 var i18n_tr = {"Actions":"Aktionen","Close":"Schlie\u00dfen","Copy url":"URL kopieren","Current directory":"Aktuelles Verzeichnis","Delete":"L\u00f6schen","Download":"Download","File":"Datei","File Browser":"Dateibrowser","File Manager":"Dateimanager","File upload":"Datei-Upload","Name":"Name","New directory":"Neues Verzeichnis","Preview":"Vorschau","Root":"Root","Select":"Ausw\u00e4hlen","URL":"URL","Access denied. Please renew your authentication!":""};
--- locales/LU/messages_po.js
+++ locales/LU/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
-/* extesion: , lang: LU */
+/* extesion: filemanager, lang: LU */
 var langCode = 'LU';
 var i18n_tr = {"Actions":"Aktiounen","Close":"Zoumachen","Copy url":"URL kop\u00e9iren","Current directory":"Aktuellen R\u00e9pertoire","Delete":"L\u00e4schen","Download":"Erooflueden","File":"Fichier","File Browser":"Dateibrowser","File Manager":"Dateimanager","File upload":"Fichier roplueden","Name":"Numm","New directory":"Naien R\u00e9pertoire","Preview":"Viirbl\u00e9ck","Root":"Root","Select":"Auswielen","URL":"URL","Access denied. Please renew your authentication!":""};
