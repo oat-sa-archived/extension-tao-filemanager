e3fda13214c3113a73d05d16b2346d86a4a75337
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-26 14:37:45 +0000

    * Update extensions manifests to include model dependancy
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@4352 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -1,25 +1,21 @@
 <?php
-
 /**
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  */
-
-
-	return array(
-		'name' => 'FileManager',
-		'description' => 'Manage media files on server',
-		'additional' => array(
-			'version' => '1.2',
-			'author' => 'CRP Henry Tudor',
-			'dependances' => array(),
-			'install' => array( 
-				'php' => dirname(__FILE__). '/install/install.php'
-			),
-
-			'classLoaderPackages' => array( 
-				dirname(__FILE__).'/actions/'
-			 )
-		)
-	);
+return array(
+	'name' => 'filemanager',
+	'description' => 'Manage media files on server',
+	'additional' => array(
+		'version' => '2.0',
+		'author' => 'CRP Henry Tudor',
+		'dependances' => array('tao'),
+		'install' => array( 
+			'php' => dirname(__FILE__). '/install/install.php'
+		),
+		'classLoaderPackages' => array( 
+			dirname(__FILE__).'/actions/'
+		 )
+	)
+);
 ?>
