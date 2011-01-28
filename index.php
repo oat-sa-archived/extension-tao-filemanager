9a0204494ef37b49a25b1cedb9d25bb04050e7e2
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-28 14:01:25 +0000

    * CLEAN DATABASE  * UPDATE WAY OF CONTSTANT LOADING  * Minor fix in the wfEngine  * Add type attr by default in the QTI object tag
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@4370 62dfeb86-a93f-4914-9d13-15c0a160985d

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
 require_once dirname(__FILE__). '/../tao/includes/class.Bootstrap.php';
 require_once 'helpers/FileUtils.class.php';
 
-$options = array('constants' => array('tao'));
-
-$bootStrap = new BootStrap('filemanager', $options);
+$bootStrap = new BootStrap('filemanager');
 $bootStrap->start();
 $bootStrap->dispatch();
 ?>
