f4b3cf5f901deff7e990b09c0c8160dba6aa503e
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-07-26 11:57:43 +0000

    PO Update
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@6493 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ locales/_raw/messages_po.js
@@ -0,0 +1,4 @@
+/* auto generated content */
+/* extesion: filemanager, lang: _raw */
+var langCode = '_raw';
+var i18n_tr = {"Access denied. Please renew your authentication!":"","Actions":"","Close":"","Copy url":"","Current directory":"","Delete":"","Download":"","File":"","File Browser":"","File Manager":"","File upload":"","Name":"","New directory":"","Preview":"","Root":"","Security issue":"","Select":"","URL":"","media size must be leather than : ":"","unable to move uploaded file":"","unknow media size":""};
