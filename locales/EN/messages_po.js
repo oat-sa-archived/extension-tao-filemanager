6b0e3827029e5318e1d406682f6ed2af3f0ea87d
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-03-21 08:42:00 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@10208 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/EN/messages_po.js
+++ locales/EN/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* lang: EN */
 var langCode = 'EN';
-var i18n_tr = {" MB":"","Access denied. Please renew your authentication!":"","Actions":"","Close":"","Copy url":"","Current directory":"","Delete":"","Download":"","File":"","File Browser":"","File Manager":"","File upload":"","file upload failed":"","Max filesize":"","media size must be less than : ":"","Name":"","New directory":"","no file uploaded":"","Preview":"","Root":"","Security issue":"","Select":"","unable to move uploaded file":"","unknow media size":"","URL":""};
+var i18n_tr = {" MB":"","Access denied. Please renew your authentication!":"","Actions":"","Copy url":"","Current directory":"","Delete":"","Download":"","File":"","File Browser":"","File Manager":"","File upload":"","file upload failed":"","Max filesize":"","media size must be less than : ":"","Name":"","New directory":"","no file uploaded":"","Preview":"","Root":"","Security issue":"","Select":"","unable to move uploaded file":"","unknow media size":"","URL":""};
--- locales/_raw/messages_po.js
+++ locales/_raw/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
 /* lang: _raw */
 var langCode = '_raw';
-var i18n_tr = {" MB":"","Access denied. Please renew your authentication!":"","Actions":"","Close":"","Copy url":"","Current directory":"","Delete":"","Download":"","File":"","File Browser":"","File Manager":"","File upload":"","file upload failed":"","Max filesize":"","media size must be less than : ":"","Name":"","New directory":"","no file uploaded":"","Preview":"","Root":"","Security issue":"","Select":"","unable to move uploaded file":"","unknow media size":"","URL":""};
+var i18n_tr = {" MB":"","Access denied. Please renew your authentication!":"","Actions":"","Copy url":"","Current directory":"","Delete":"","Download":"","File":"","File Browser":"","File Manager":"","File upload":"","file upload failed":"","Max filesize":"","media size must be less than : ":"","Name":"","New directory":"","no file uploaded":"","Preview":"","Root":"","Security issue":"","Select":"","unable to move uploaded file":"","unknow media size":"","URL":""};
