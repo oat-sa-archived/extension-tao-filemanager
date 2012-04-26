642c93890a7130e40e9c449892f687eed0574285
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-04-26 14:27:31 +0000

    Full update of the platform for translations: - PO Files containing source code messages. - RDF Translation Models for Ontologies.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@10936 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/DE/messages_po.js
+++ locales/DE/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
-/* lang: DE */
-var langCode = 'DE';
+/* lang: EN */
+var langCode = 'EN';
 var i18n_tr = {" MB":"","Access denied. Please renew your authentication!":"","Actions":"Aktionen","Copy url":"URL kopieren","Current directory":"Aktuelles Verzeichnis","Delete":"L\u00f6schen","Download":"Download","File":"Datei","File Browser":"Dateibrowser","File Manager":"Dateimanager","File upload":"Datei-Upload","file upload failed":"","Max filesize":"","media size must be less than : ":"","Name":"Name","New directory":"Neues Verzeichnis","no file uploaded":"","Preview":"Vorschau","Root":"Root","Security issue":"","Select":"Ausw\u00e4hlen","unable to move uploaded file":"","unknow media size":"","URL":"URL"};
--- locales/LU/messages_po.js
+++ locales/LU/messages_po.js
@@ -1,4 +1,4 @@
 /* auto generated content */
-/* lang: LU */
-var langCode = 'LU';
+/* lang: EN */
+var langCode = 'EN';
 var i18n_tr = {" MB":"","Access denied. Please renew your authentication!":"","Actions":"Aktiounen","Copy url":"URL kop\u00e9iren","Current directory":"Aktuellen R\u00e9pertoire","Delete":"L\u00e4schen","Download":"Erooflueden","File":"Fichier","File Browser":"Dateibrowser","File Manager":"Dateimanager","File upload":"Fichier roplueden","file upload failed":"","Max filesize":"","media size must be less than : ":"","Name":"Numm","New directory":"Naien R\u00e9pertoire","no file uploaded":"","Preview":"Viirbl\u00e9ck","Root":"Root","Security issue":"","Select":"Auswielen","unable to move uploaded file":"","unknow media size":"","URL":"URL"};
