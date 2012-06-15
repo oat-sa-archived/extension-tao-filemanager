aff208b7ad97881456496c1b2d4dc91654ca8c78
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-06-15 12:19:16 +0000

    updated extension nrs to 2.3 removed force smooth for versions pre 2.1
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@11488 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -7,7 +7,7 @@ return array(
 	'name' => 'filemanager',
 	'description' => 'Manage media files on server',
 	'additional' => array(
-		'version' => '2.0',
+		'version' => '2.3',
 		'author' => 'CRP Henry Tudor',
 		'dependances' => array('tao'),
 		'classLoaderPackages' => array(
