6cf4a80eba110af0adb148d8952a6366d334040d
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-11-21 11:27:31 +0000

    filemanager::helpers::FileUtils added to argoUML.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@12842 62dfeb86-a93f-4914-9d13-15c0a160985d

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  */
 require_once dirname(__FILE__). '/../tao/includes/class.Bootstrap.php';
-require_once 'helpers/FileUtils.class.php';
 
 $bootStrap = new BootStrap('filemanager');
 $bootStrap->start();
