5ebc3ad041c46862199d39c0c4efd20af724b2f4
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-01-28 15:01:43 +0000

    new users management, 1st encounter
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/filemanager/trunk@13546 62dfeb86-a93f-4914-9d13-15c0a160985d

--- test/index.php
+++ test/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . '/TaoTestRunner.php';
+require_once dirname(__FILE__) . '/../../tao/test/TaoTestRunner.php';
 
 //get the test into each extensions
 $tests = TaoTestRunner::getTests(array('filemanager'));
